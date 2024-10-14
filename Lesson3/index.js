c53d82cae42f34fafd349d791eb156894d0ea104
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-10-14 19:04:27 +0300

    + pageLook

---
+++ Lesson3/index.js
@@ -0,0 +1,40 @@
+async function getData() {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    return data;
+}
+
+async function handleData() {
+    const data = await getData();
+    
+    showData(data.results);
+}
+function showData(data) {
+    const root = document.getElementById('character grid');
+    data.forEach(character => {
+        showCharacter(root, character);
+    });
+}
+
+function showCharacter(root, character) {
+    const div = document.createElement('div');
+    div.className = 'character';
+
+    const img = document.createElement('img');
+    img.src = character.image;
+    div.appendChild(img);
+
+    const name = document.createElement('p');
+    name.className = 'name';
+    name.textContent = character.name;
+    div.appendChild(name);
+
+    const ifAlive = document.createElement('p');
+    ifAlive.className = 'alive';
+    ifAlive.textContent = character.status;
+    div.appendChild(ifAlive);
+
+    root.appendChild(div);
+}
+
+handleData();
