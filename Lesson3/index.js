5cd88dd284443eb6c939d8282c62bc4cf9e815f3
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-10-14 19:40:07 +0300

    + pagination

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -1,19 +1,32 @@
-async function getData() {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
+async function getData(url) {
+    const response = await fetch(url);
     const data = await response.json();
     return data;
 }
 
-async function handleData() {
-    const data = await getData();
-    
-    showData(data.results);
+async function handleData(url = 'https://rickandmortyapi.com/api/character') {
+    const data = await getData(url);
+    showData(data);
 }
+
 function showData(data) {
-    const root = document.getElementById('character grid');
-    data.forEach(character => {
+    const root = document.querySelector('.container');
+    const loading = document.querySelector('.loading');
+    root.innerHTML = '';
+
+    root.style.display = 'none';
+    loading.style.display = 'block';
+
+    data.results.forEach(character => {
         showCharacter(root, character);
     });
+
+    loading.style.display = 'none';
+    root.style.display = 'grid';
+    
+    showPageNumber(data);
+
+    setupPagination(data);
 }
 
 function showCharacter(root, character) {
@@ -41,6 +54,41 @@ function showCharacter(root, character) {
     div.appendChild(ifAlive);
 
     root.appendChild(div);
+}
+
+function showPageNumber(data) {
+    const pageNumber = document.getElementById('pageNumber');
+    if(data.info.next){
+        const urlObj = new URL(data.info.next);
+        const params = new URLSearchParams(urlObj.search);
+        const page = params.get('page');
+        pageNumber.textContent = page - 1;
+    }
+    else{
+        pageNumber.textContent = data.info.pages;
+    }
+}
+
+function setupPagination(data) {
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+
+    prevButton.onclick = null;
+    nextButton.onclick = null;
+
+    if (data.info.prev) {
+        prevButton.disabled = false;
+        prevButton.onclick = () => handleData(data.info.prev);
+    } else {
+        prevButton.disabled = true; 
+    }
+
+    if (data.info.next) {
+        nextButton.disabled = false;
+        nextButton.onclick = () => handleData(data.info.next);
+    } else {
+        nextButton.disabled = true;
+    }
 }
 
 handleData();
