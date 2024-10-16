8d28a37590c23670cfbce26bde4faf1cf2cfe0a5
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-10-16 16:02:09 +0300

    + modal

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -1,43 +1,69 @@
 const root = document.querySelector('.container');
 const loading = document.querySelector('.loading');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.close');
 
-async function getData(url) {
+const getData = async (url) => {
     const response = await fetch(url);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     const data = await response.json();
     return data;
-}
+};
 
-async function handleData(url = 'https://rickandmortyapi.com/api/character') {
-    showData(await getData(url));
-}
-
-function showData(data) {
+const showData = async (url = 'https://rickandmortyapi.com/api/character')  => {
+    const data = await getData(url);
     root.innerHTML = '';
     root.style.display = 'none';
     loading.style.display = 'block';
 
-    data.results.forEach(character => showCharacter(root, character));
+    addData(data);
     loading.style.display = 'none';
     root.style.display = 'grid';
-    
-    showPageNumber(data);
-    setupPagination(data);
-}
+};
 
-function showCharacter(root, { image, name, status }) {
-    const div = document.createElement('div');
-    div.className = 'character';
-    
+const addData = ({ results }) => {
+    const charactersHtml = results.map(character => showCharacter(character)).join('');
+    root.innerHTML += charactersHtml;
+};
+
+const showModal = async (url) => {
+    const characterData = await getData(url);
+    modalContent.innerHTML = `
+        <img src="${characterData.image}">
+        <p class="name">${characterData.name}</p>
+    `;
+    modal.style.display = 'flex';
+};
+
+const showCharacter = ({ image, name, status, id }) => {
     const statusColor = status === 'Alive' ? 'limegreen' : status === 'Dead' ? 'red' : 'gray';
 
-    div.innerHTML = `
-        <img src="${image}">
-        <p class="name">${name}</p>
-        <p class="alive" style="color: ${statusColor}">${status}</p>
+    return `
+        <div class="character" data-index="${id}">
+            <img src="${image}">
+            <p class="name">${name}</p>
+            <p class="alive" style="color: ${statusColor}">${status}</p>
+        </div>
     `;
+};
 
-    root.appendChild(div);
-}
+root.addEventListener('click', (event) => {
+    const character = event.target.closest('.character');
+    if (!character) return;
+    const index = character.getAttribute('data-index');
+    const url = `https://rickandmortyapi.com/api/character/${index}`;
+    showModal(url);
+});
 
-handleData();
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+showData();
