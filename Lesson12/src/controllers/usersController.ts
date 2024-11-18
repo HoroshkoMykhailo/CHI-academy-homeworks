4a564bbac89fc9e4b5dde7fbcf30e163e35f19e2
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-11-18 13:38:03 +0200

    feat: add createUser endpoint

--- Lesson12/src/controllers/usersController.ts
+++ Lesson12/src/controllers/usersController.ts
@@ -1,7 +1,28 @@
 import { Request, Response } from 'express';
-import { readUsersFromFile } from '../utils/fileHandler';
+import { readUsersFromFile, writeUsersToFile } from '../utils/fileHandler';
+import { User } from '../types/User';
 
 export const getUsers = (req: Request, res: Response): void => {
     const users = readUsersFromFile();
     res.json(users);
+};
+
+export const createUser = (req: Request, res: Response): void => {
+    const { user, email } = req.body;
+
+    if (!user || !email) {
+        res.status(400).json({ error: 'User and email are required' });
+        return;
+    }
+
+    const users = readUsersFromFile();
+    const newUser: User = { id: Date.now(), user, email };
+    if(users.some(user => user.email === email)) {
+        res.status(400).json({ error: 'User with this email already exists' });
+        return;
+    }
+    users.push(newUser);
+    writeUsersToFile(users);
+
+    res.status(201).json(newUser);
 };
